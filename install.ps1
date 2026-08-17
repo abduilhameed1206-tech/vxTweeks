81b7449d734487e1108bf56436ebb465c1144821
abduilhameed1206-tech <abduilhameed1206@gmail.com>
2026-08-16 17:24:58 -0700

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -1,7 +1,26 @@
+$ErrorActionPreference = "Stop"
+
+$url = "https://github.com/abduilhameed1206-tech/vxTweeks/releases/download/v1.0/vxTweeks.exe"
 $temp = "$env:TEMP\vxTweeks.exe"
 
-Invoke-WebRequest `
--Uri "https://github.com/abduilhameed1206-tech/vxTweeks/releases/download/v1.0/vxTweeks.exe" `
--OutFile $temp
+Write-Host "Downloading VX TWEeks..." -ForegroundColor Cyan
 
-Start-Process $temp -Verb RunAs
+Invoke-WebRequest `
+    -Uri $url `
+    -OutFile $temp
+
+if (!(Test-Path $temp)) {
+    Write-Host "Failed to download VX TWEeks." -ForegroundColor Red
+    exit 1
+}
+
+Write-Host "Download completed." -ForegroundColor Green
+
+# إزالة Mark of the Web
+Unblock-File -Path $temp -ErrorAction SilentlyContinue
+
+Write-Host "Starting VX TWEeks as Administrator..." -ForegroundColor Cyan
+
+Start-Process `
+    -FilePath $temp `
+    -Verb RunAs
