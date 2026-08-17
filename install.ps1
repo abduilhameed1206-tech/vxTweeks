43c651a8d20cc5233c88a65d4347e8872d56999b
abduilhameed1206-tech <abduilhameed1206@gmail.com>
2026-08-16 17:17:31 -0700

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 $temp = "$env:TEMP\vxTweeks.exe"
 
 Invoke-WebRequest `
--Uri "https://github.com/abduilhameed1206-tech/vxTweeks/blob/main/vxTweeks.exe" `
+-Uri "https://github.com/abduilhameed1206-tech/vxTweeks/releases/download/v1.0/vxTweeks.exe" `
 -OutFile $temp
 
 Start-Process $temp -Verb RunAs
